f67be9e364966a2caeb6893f7d1d4f434af90c90
Robson Ð. Oliveira <robsonedoliveira@gmail.com>
2023-07-14 09:08:41 -0300

    14/07/2023
    14/07/2023

--- CSHARP.js
+++ CSHARP.js
@@ -8,6 +8,91 @@ writer.WriteLine("1 _user - " + _user);
 
 writer.Close();
 ______________________________________________________________________________________________________________
+# QUANDO OCORRER ERRO REFERENTE A DATA, FUNCIONA BEM LOCAL E NÃO NO SERVIDOR.
+# COLOCAR ESSE TRECHO NA CLASSSE (Program.cs)
+
+// Get Culture Info from appsettings and set Default culture for application.
+// If not found culture value from appsettings then use "pt-BR"
+var culture = builder.Configuration.GetValue<string>("SetCultureInfo:Culture") ?? "pt-BR";
+var cultureInfo = new System.Globalization.CultureInfo(culture);
+System.Globalization.CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+System.Globalization.CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+______________________________________________________________________________________________________________
+# PARA COOCKIES NA CLASSE (Program.cs)
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>{
+        //options.Cookie.HttpOnly = true;
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+        options.LoginPath = "/Login/Index";
+        options.AccessDeniedPath = "/Login/Index";
+        options.LogoutPath = "/Login/Logout";
+        options.SlidingExpiration = true;
+    });
+______________________________________________________________________________________________________________
+# REMOVER A PALAVRA "AspNet" DAS TABELAS DO IDENTITY NO BANCO
+
+ protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Corrigir o nome das tabelas do Identity
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                var table = entityType.GetTableName();
+                if (table.StartsWith("AspNet"))
+                    entityType.SetTableName("" + table.Substring(6));
+
+                foreach (var fk in entityType.GetForeignKeys())
+                         fk.DeleteBehavior = DeleteBehavior.NoAction;
+            }
+		}
+
+
+
+______________________________________________________________________________________________________________
+# Necessário para executar o arquivo .Bat do sistema
+C:\KenanFX\FXLauncher\FXLauncher_x32
+
+            Process proc = null;
+
+            string caminhoCompleto = @_url;
+            string _caminhoCompleto = Path.GetDirectoryName(caminhoCompleto);
+            string _nomeArquivo = Path.GetFileName(caminhoCompleto);            
+            string _batDir = string.Format(@_caminhoCompleto);
+
+            proc = new Process();
+            proc.StartInfo.WorkingDirectory = _batDir;
+            proc.StartInfo.FileName = _nomeArquivo; // bat
+            proc.StartInfo.CreateNoWindow = false;
+            proc.Start();
+            proc.WaitForExit();
+           
+            proc.Close();
+______________________________________________________________________________________________________________
+# ESPERAR POR UMA JANELA
+
+            // Espero a tela [Login] aparecer
+            string sJanelaDeEspera = "";
+            while (sJanelaDeEspera == "")
+            {
+                if (AutoItX.WinExists(sNomeJanela) == 1)
+                {
+                    sJanelaDeEspera = sNomeJanela;
+
+                    // Insiro Usuário
+                    Thread.Sleep(500);
+                    AutoItX.Send(_user + "{TAB}");
+
+                    // Insiro Senha
+                    Thread.Sleep(500);
+                    AutoItX.Send(sSenhaTratada + "{TAB}{Enter}");
+
+                    break;
+                }
+            }
+______________________________________________________________________________________________________________
+______________________________________________________________________________________________________________
+______________________________________________________________________________________________________________
 # PEGAR CAMINHO DA PASTA DO USUÁRIO DE REDE
 string userPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
 string ramal = "";
@@ -105,6 +190,8 @@ AutoItX.ControlSend("Ramal:", "", "[CLASSNN:WindowsForms10.EDIT.app.0.378734a1]"
 
 CLICAR
 AutoItX.ControlClick("Ramal:", "", "[Name:btLogin]");
+AutoItX.ControlClick(sNomeJanela, "", "[CLASS:TBitBtn]");
+AutoItX.ControlClick(sJanelaLogin, "", "[CLASS:Button; INSTANCE:1]");
 
 ALT + A
 AutoItX.Send("{ALT}{ASC 97}");
@@ -244,3 +331,149 @@ ________________________________________________________________________________
             return true;
         }
 ______________________________________________________________________________________________________________
+# WHILE COM TIMER  
+  
+  long startTime = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+  string pageTitle = "Entrar em sua conta"; // Título da página
+  int pageIsActive;
+ 
+ while (true)
+	{
+		pageIsActive = AutoItX.WinExists("[TITLE:" + pageTitle + "]");
+
+		if (pageIsActive == 1)
+		{
+			// FAÇA
+			break;
+		}
+		else
+		{
+			// Pequena pausa se a página não estiver ativa
+			Thread.Sleep(1000);
+		}
+
+		// Obtém o tempo atual em milissegundos
+		long currentTime = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+
+		// Verifica se o tempo de execução excedeu 1 minuto e pouco (80000 segundos) e sai do loop
+		if ((currentTime - startTime) >= 20000)
+			break;
+
+		// Aguarda 1 segundo antes de verificar novamente
+		Thread.Sleep(1000);
+	}
+______________________________________________________________________________________________________________
+# TIMER PARA AGUARDAR A PÁGINA CARREGAR
+
+     int timeout = 80000; // tempo limite em milissegundos (80 segundos)
+     DateTime start = DateTime.Now; // horário atual
+     
+  while (AutoItX.WinExists(pageTitle) != 1)
+    {
+       // Se o tempo limite for atingido, interrompa o loop
+      if ((DateTime.Now - start).TotalMilliseconds > timeout)
+         {
+             writer.WriteLine("Página não exibida após {0} segundos.", timeout / 1000);
+             break;
+         }
+
+        // Aguarde 1 segundo antes de verificar novamente
+        Thread.Sleep(1000);
+    }
+______________________________________________________________________________________________________________
+# AUTOMAÇÃO AUTOITX
+
+                // INJETAR CÓDIGO VIA CONSOLE NAVEGADOR
+                AutoItX.WinWait(sNomeJanela, "", 20);
+                AutoItX.WinActivate(sNomeJanela, "");
+                Thread.Sleep(100);
+
+                // Abre o console do Chrome e injeta o código JavaScript
+                AutoItX.Send("^+j"); // Abre o console do Chrome
+                Thread.Sleep(500);
+                AutoItX.WinActivate(sNomeJanela, "");
+                Thread.Sleep(500);
+                AutoItX.Send("document.evaluate('/html/body/section[3]/div[2]/section[5]/div[5]/div/ul/li[22]/a[4]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click();");
+                Thread.Sleep(2000);
+                AutoItX.Send("{ENTER}");
+                Thread.Sleep(500);
+                AutoItX.Send("^+j"); // Fecha o console do Chrome
+				
+				// FOCO NA BARRA DE ENDEREÇO
+				Thread.Sleep(2000);
+                AutoItX.Send("^l");
+
+                // Apagando a url antiga na barra de navegação
+                AutoItX.Send("{BACKSPACE}");
+______________________________________________________________________________________________________________
+# OCULTAR BOTÃO MOSTRAR SENHA GOOGLE CHROME
+
+ // Ocultar ícone mostrar senha
+	Thread.Sleep(200);
+	string ocultarMostrarSenha = "const elementToRemove = document.evaluate('/html/body/div/div[2]/div/div/div/div/div/div/div/div[2]/div/form/div[2]/span/div/div[1]/div[2]/button', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;elementToRemove.remove();";
+	_chrome.RunJavascript(ocultarMostrarSenha);
+	
+______________________________________________________________________________________________________________
+# Abro uma nova Aba no mesmo navegador
+                Thread.Sleep(7000);
+                AutoItX.Send("^{t}");
+
+
+
+
+
+
+
+  var sNomeJanela = "PontoBR Sistemas";
+            AutoItX.WinWait(sNomeJanela, "", 20);
+            AutoItX.WinActivate(sNomeJanela, "");
+
+            string title = Process.GetCurrentProcess().MainWindowTitle;
+
+            // Verifica se o título da janela contém a string "DevTools"
+            if (title.Contains("DevTools - "))
+            {
+                Console.WriteLine("O console do DevTools está ativo na página atual.");
+            }
+            else
+            {
+                Console.WriteLine("O console do DevTools não está ativo na página atual.");
+            }
+			
+			
+			
+
+______________________________________________________________________________________________________________
+string pageTitle = @"SAP Logon 730 - \\Remote";
+AutoItX.WinWait(pageTitle, "", 20);
+AutoItX.WinActivate(pageTitle, "");
+
+// Aguarda a nova página abrir
+AutoItX.WinWaitActive(pageTitle, "", 30);
+
+Na linha adicionada ao código acima, a função WinWaitActive() aguarda a janela especificada ser ativada por até 30 segundos antes de continuar com a execução do código. Isso permite que o código aguarde a nova página ser aberta sem ficar em um loop infinito, aumentando assim a robustez da solução.
+______________________________________________________________________________________________________________
+# EVENTOS NO FORM
+
+# Clica no ComboBox
+AutoItX.ControlClick("", "", "[CLASS:TsuiDBLookupComboBox; INSTANCE:1]");
+
+# Copiar algum texto no form
+string copiarTexto = AutoItX.ControlGetText("Integrador", "", "[CLASS:WindowsForms10.STATIC.app.0.13965fa_r7_ad1; INSTANCE:2]").Replace("Código: ", "");
+
+sNomeJanela = "Ramal:";
+AutoItX.WinWait(sNomeJanela, "", 20);
+AutoItX.WinActivate(sNomeJanela, "");
+AutoItX.ControlSend("Ramal:", "", "[CLASSNN:WindowsForms10.EDIT.app.0.378734a1]", copiarTexto);
+
+# Clica no botão
+AutoItX.ControlClick("Ramal:", "", "[Name:btLogin]");
+______________________________________________________________________________________________________________
+
+
+
+
+
+
+
+
---
+++ LocadoraDeCarros/Presentacao/LocadoraDeCarros/Controllers/ClienteController.cs
@@ -0,0 +1,173 @@
+﻿using AutoMapper;
+using LocadoraDeCarros.Controllers.Base;
+using LocadoraDeCarros.Models;
+using Microsoft.AspNetCore.Mvc;
+using Negocio.Models;
+using Negocio.ServicoNegocio.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LocadoraDeCarros.Controllers
+{
+    public class ClienteController : BaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly IClienteServico _clienteServico;
+
+        public ClienteController(IMapper mapper, IClienteServico clienteServico)
+        {
+            _mapper = mapper;
+            _clienteServico = clienteServico;
+        }
+
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Cliente/Details/5
+        public ActionResult Details(int id)
+        {
+            ClienteViewModel clienteVM = _mapper.Map<ClienteViewModel>(_clienteServico.ObterClientePorID(id));
+            return View(clienteVM);
+        }
+
+        // GET: Cliente/Create
+        public ActionResult Create()
+        {
+            return View(new ClienteViewModel());
+        }
+
+        // POST: Cliente/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ClienteViewModel clienteVM)
+        {
+            try
+            {
+
+                var novoCliente = _mapper.Map<Cliente>(clienteVM);
+
+                //TODO: validar regras de negocio
+                if (novoCliente.EmailEstaDuplicado(_clienteServico))
+                    ModelState.AddModelError("Email", "O email ja existe no banco de dados");
+
+                if (ModelState.IsValid)
+                {
+                    if (_clienteServico.Inserir(novoCliente))
+                    {
+                        Mensagem("O cliente foi inserido com sucesso.", "Info");
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+                Mensagem("Ocorreu algum erro ao inserir o novo cliente.", "Error");
+                return View(clienteVM);
+            }
+            catch
+            {
+                Mensagem("Ocorreu alguma exception ao inserir o novo cliente.", "Error");
+                return View(clienteVM);
+            }
+        }
+
+        // GET: Cliente/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var clienteEditar = _mapper.Map<ClienteViewModel>(_clienteServico.ObterClientePorID(id));
+            return View(clienteEditar);
+        }
+
+        // POST: Cliente/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ClienteViewModel clienteVM)
+        {
+            try
+            {
+
+                var clienteEditado = _mapper.Map<Cliente>(clienteVM);
+
+                if (clienteEditado.EmailEstaDuplicado(_clienteServico))
+                    ModelState.AddModelError("Email", "O email ja existe no banco de dados");
+
+                if (ModelState.IsValid)
+                {
+
+                    if (_clienteServico.Editar(clienteEditado))
+                    {
+                        Mensagem("O cliente foi editado com sucesso.", "Info");
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+
+                Mensagem("Ocorreu algum erro ao editar o cliente.", "Error");
+                return View(clienteVM);
+            }
+            catch
+            {
+                Mensagem("Ocorreu alguma exception ao editar o cliente.", "Error");
+                return View(clienteVM);
+            }
+        }
+
+        // GET: Cliente/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var clienteExcluir = _mapper.Map<ClienteViewModel>(_clienteServico.ObterClientePorID(id));
+            return View(clienteExcluir);
+        }
+
+        // POST: Cliente/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(ClienteViewModel clienteExcluir)
+        {
+            try
+            {
+                if (_clienteServico.Excluir(clienteExcluir.id))
+                {
+                    Mensagem("O cliente foi excluido com sucesso.", "Info");
+                    return RedirectToAction(nameof(Index));
+                }
+
+                Mensagem("Ocorreu alguma erro ao excluir o cliente.", "Error");
+                return View(clienteExcluir);
+            }
+            catch
+            {
+                Mensagem("Ocorreu alguma exception ao excluir o cliente.", "Error");
+                return View(clienteExcluir);
+            }
+        }
+
+        [HttpPost, ActionName("CarregarDados")]
+        public ActionResult CarregarDados() 
+        {
+            var draw = Request.Form["draw"];
+            var start = Request.Form["start"];
+            var length = Request.Form["length"];
+            var sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"] + "][name]"];
+            var sortColumnDir = Request.Form["order[0][dir]"];
+            var searchValue = Request.Form["search[value]"];
+
+            int pageSize = length != string.Empty ? Convert.ToInt32(length) : 0;
+            int skip = start != string.Empty ? Convert.ToInt32(start) : 0;
+            int recordsTotal = 0;
+
+            var result = _clienteServico.ObterListaClientes();
+
+            if (!string.IsNullOrEmpty(searchValue))
+            {
+                result = (List<Cliente>)result.Where(m => m.Nome.Contains(searchValue)).ToList();
+            }
+
+            recordsTotal = result.Count();
+
+            var data = result.Skip(skip).Take(pageSize).ToList();
+
+            return Json(new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = data });
+        }
+    }
+}
